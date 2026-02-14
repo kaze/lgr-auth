ec263d1a9991d5421725d7f8d11d79265a50d7bb
zsolt kormany <zsoltkormany@gmail.com>
2026-02-14 12:39:28 +0100

    [p1t4] - implement routes using tdd

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -1,5 +1,8 @@
 use std::error::Error;
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
 use axum::Router;
+use axum::routing::{get, post};
 use axum::serve::Serve;
 use tokio::net::TcpListener;
 use tower_http::services::ServeDir;
@@ -19,7 +22,13 @@ impl Application {
         // We don't need it at this point!
         let assets_dir = ServeDir::new("assets");
         let router = Router::new()
-                .fallback_service(assets_dir);
+                .fallback_service(assets_dir)
+                .route("/signup", get(signup))
+                .route("/signup", post(signup))
+                .route("/login", post(login))
+                .route("/logout", post(logout))
+                .route("/verify-2fa", post(verify_2fa))
+                .route("/verify-token", post(verify_token));
 
         let listener = tokio::net::TcpListener::bind(address).await?;
         let address = listener.local_addr()?.to_string();
@@ -34,4 +43,24 @@ impl Application {
         println!("listening on {}", &self.address);
         self.server.await
     }
+}
+
+async fn signup() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn login() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn logout() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn verify_2fa() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn verify_token() -> impl IntoResponse {
+    StatusCode::OK.into_response()
 }
